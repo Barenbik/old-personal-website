f8c2f39a48b25c40018b32e33816ca1efb48d428
Tony Sharples <tonysharples22@me.com>
2022-05-26 18:49:27 +0100

    Add fading elements

---
+++ index.js
@@ -0,0 +1,18 @@
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.3,
+};
+
+function observerCallback(entries, _) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.replace('fade-out', 'fade-in');
+    }
+  });
+}
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+const fadeElements = document.querySelectorAll('.fade-element');
+
+fadeElements.forEach((el) => observer.observe(el));
